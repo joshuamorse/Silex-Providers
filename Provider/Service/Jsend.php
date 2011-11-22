2d60ad24ea50b0a13f34087ec8ef3f931978de98
Joshua Morse <joshua.morse@iostudio.com>
2011-11-22 08:19:42 -0600

    restricting status usage to the 3 defined by jsend in the jsend provider

--- Provider/Service/Jsend.php
+++ Provider/Service/Jsend.php
@@ -16,9 +16,22 @@ use Jmflava\Provider\Service\JSON;
  */
 class Jsend implements ServiceProviderInterface
 {
+    /**
+     * Defines the allowed Jsend statuses. 
+     */
+    private $allowed_statuses = array('success', 'fail', 'error');
+
     public function register(Application $app)
     {
-        $app['jsend_response'] = $app->protect(function ($status = null, array $data, $status_code = 200) use ($app) {
+        $allowed_statuses = $this->allowed_statuses;
+
+        $app['jsend_response'] = $app->protect(function ($status = null, array $data, $status_code = 200) use ($app, $allowed_statuses) {
+            if (!in_array($status, $allowed_statuses)) {
+                return new Response(500, sprintf('Jsend only supports the following 3 statuses: %s.',
+                    implode(', ', $allowed_statuses)
+                ));
+            }
+
             $jsend_data = array(
                 'status' => $status,
                 'data' => $data,
